54a0b9f5df40a7a0eeb8a4d39bdf86c3db8946e1
shry9735 <triguyprogramming@gmail.com>
2024-02-21 10:34:11 -0800

    Beacon Hardware Test
    Created beacon.h to define pinout of the the beacon itself, also created a test script that toggles everything while mirroring Nanotron communication.
    
    Still need to come through and put everything into a separate file that can be dropped into a new program. Will get there.

---
+++ Beacon/Beacon_Hardware_Test/Beacon_Hardware_Test.ino
@@ -0,0 +1,190 @@
+#include "beaconpinout.h"
+
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  if(beaconInit())
+  {
+    goodFlash();
+  }
+  else
+  {
+    errorFlash();
+  }
+  beaconFlash();
+  goodFlash();
+}
+
+// the loop function runs over and over again forever
+int loopcount=0;
+void loop() {
+  
+  if((loopcount%30000)==0)
+  {
+    Serial.println("Running, you can input a command for the nanotron to test, make sure that Both NL & CR is selected");
+    beaconFlash();
+    loopcount=0;
+  }
+
+  if (Serial.available()) 
+  {       
+    char buff[32];
+    int i=0;
+    while(Serial.available())
+    {
+      buff[i]=Serial.read();
+      i++;
+      delay(1);
+    }
+    for(int j=0;j<i;j++)
+    {
+      Serial2.print(buff[j]);  // read it and send it out Serial1 (pins 0 & 1)
+      Serial.print(buff[j]);
+    }
+
+  }
+
+  if (Serial2.available())
+  {
+    while(Serial2.available())
+    {
+      Serial.print(Serial2.read());
+      delay(1);//stops messages from getting sliced.
+    }
+    Serial.println();
+  }
+  
+  loopcount++;
+  delay(1);
+
+}
+
+bool beaconInit()
+{
+  bool startupStatus=true;
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+
+  pinMode(PORTPOWER,OUTPUT);
+
+  pinMode(RELAYIN1,INPUT);
+  pinMode(RELAYIN2,INPUT);
+
+  pinMode(RELAYOUT1, OUTPUT);
+  pinMode(RELAYOUT2, OUTPUT);
+
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  digitalWrite(LED5,LOW);
+  digitalWrite(RELAYOUT1,LOW);
+  digitalWrite(RELAYOUT2,LOW);
+
+  Serial.begin(BEACONBAUD);
+  Serial2.begin(NANOBAUD); //Nanotron Communication
+  Serial2.println("GNID");
+  int timeout=0;
+  while(!Serial2.available()&&timeout<10)
+  {
+    timeout++;
+    delay(1);
+  }
+  if (Serial2.available()) 
+  {       
+    Serial.print("Nanotron communication detected, got:");
+    while(Serial2.available())
+    {
+      Serial.write(Serial2.read());
+      delay(1);  //a small delay helps the entire message come through before moving on. Otherwise messages got cut in half
+    }
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("Nanotron communication failed,  timeout");
+    startupStatus=false;//no nanotron communication was received, even after waiting
+  }
+
+return startupStatus;
+}
+
+void beaconFlash()
+{
+  digitalWrite(RELAYOUT1,HIGH);
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(LED4,HIGH);
+  digitalWrite(LED5,HIGH);
+  delay(1000);
+  digitalWrite(RELAYOUT1,LOW);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,LOW);
+  digitalWrite(LED5,LOW);
+  delay(100);
+  digitalWrite(RELAYOUT2,HIGH);
+  delay(1000);
+  digitalWrite(RELAYOUT2,LOW);
+}
+
+void errorFlash()
+{
+  for(int i =0;i<5;i++)
+  {
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,HIGH);
+    delay(250);
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,HIGH);
+    digitalWrite(LED5,LOW);
+    delay(250);
+  }
+
+}
+
+void goodFlash()
+{
+  digitalWrite(LED1,HIGH);
+  delay(100);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  delay(100);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,HIGH);
+  delay(100);
+  digitalWrite(LED3,LOW);
+  digitalWrite(LED4,HIGH);
+  delay(100);
+  digitalWrite(LED4,LOW);
+  digitalWrite(LED5,HIGH);
+  delay(100);
+  digitalWrite(LED5,LOW);
+  for(int i =0;i<2;i++)
+  {
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,HIGH);
+    digitalWrite(LED4,HIGH);
+    digitalWrite(LED5,HIGH);
+    delay(250);
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,LOW);
+    delay(250);
+  }
+}
---
+++ Beacon/Beacon_Hardware_Test/beaconpinout.h
@@ -0,0 +1,38 @@
+#define LED1 30 //defined as output in beaconinit
+#define LED2 31//defined as output in beaconinit
+#define LED3 32//defined as output in beaconinit
+#define LED4 33//defined as output in beaconinit
+#define LED5 34//defined as output in beaconinit
+
+#define SX1280LED 8
+#define SX1280CS 10
+#define SX1280RST 9
+#define SX1280BUSY 7
+#define SX1280DIO1 5
+
+#define FLASHCS 21
+
+#define PORTPOWER 22//defined as output in beaconinit
+#define PORTCOM1 3
+#define PORTCOM2 4
+
+#define BUZZER 20
+
+#define RELAYIN1 18//defined as input in beaconinit
+#define RELAYIN2 19//defined as input in beaconinit
+
+#define RELAYOUT1 14//defined as output in beaconinit
+#define RELAYOUT2 15//defined as output in beaconinit
+
+#define NANOTX 17
+#define NANORX 16
+#define NANOBAUD 115200
+
+#define BEACONBAUD 115200
+
+#define ANALOGAUX1 A8
+#define ANALOGAUX2 A9
+#define ANALOGAUX3 A10
+#define ANALOGAUX4 A11
+#define ANALOGAUX5 A12
+
