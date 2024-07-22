06bda58db5def04a5f859caeb8514b8c304b2822
Jacob-Karl <karljacobammon@gmail.com>
2024-07-22 10:59:14 -0700

    stub out new initial architecture

---
+++ Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/beacon_settings.h
@@ -0,0 +1,60 @@
+/*
+Last Edited 7/22/24
+By: Matthew Shryock, Jacob Karl
+*/
+
+#define LED1 30 //defined as output in beaconinit
+#define LED2 31//defined as output in beaconinit
+#define LED3 32//defined as output in beaconinit
+#define LED4 33//defined as output in beaconinit
+#define LED5 34//defined as output in beaconinit
+
+#define SX1280LED 8
+#define SX1280CS 10
+#define SX1280RST 9
+#define SX1280BUSY 7
+#define SX1280DIO1 5
+
+#define FLASHCS 21
+
+#define PORTPOWER 22//defined as output in beaconinit
+#define PORTCOM1 3
+#define PORTCOM2 4
+
+#define BUZZER 20
+
+#define RELAYIN1 18//defined as input in beaconinit
+#define RELAYIN2 19//defined as input in beaconinit
+
+#define RELAYOUT1 14//defined as output in beaconinit
+#define RELAYOUT2 15//defined as output in beaconinit
+
+#define NANOTX 17
+#define NANORX 16
+#define NANOBAUD 115200
+
+#define BEACONBAUD 115200
+
+#define ANALOGAUX1 A8
+#define ANALOGAUX2 A9
+#define ANALOGAUX3 A10
+#define ANALOGAUX4 A11
+#define ANALOGAUX5 A12
+
+
+struct Tag {
+  String ID; //the unique ID of this tag
+  float distance = -1; //the distance of the tag from the boundary when we ranged it last. Initialized to -1 to prevent us from warning a tag that just entered the network.
+  int cooldown_timestamp = 0; //the next time we know the tag will be awake.
+  int[64] encryption_key; //this tags unique encryption_key. One of the 64 encryption keys from the encryption_key_table.
+  bool warning_flag = false; //a switch that tells us whether or not ro sent the reset signal rather than do nothing when a tag is outside the boundary.
+  int communication_attempts = 0; //the number of times we have tried to communicate with this tag without getting a response.
+}
+
+float boundary = 100.0; //the distance from the beacon that tags will start to recieve warnings.
+Tag[] all_tags; //a list of tag structures that constitutes all of the tags on the network.
+int[256] message; //the CDMA encoded message that we broadcast at the end of the main loop.
+int[64][64] encryption_key_table; //a set of 64 orthoganal binary encryption codes (Walsh matricies, maybe, I need to look into this).
+int max_communication_attempts = 10; //the number of times we will try and communicate with a tag before we remove it from the network.
+
+
---
+++ Tag/nanoTron_Tag/nanoTron_Tag/tag_settings.h
@@ -0,0 +1,44 @@
+/*
+This file provides a pinout for the virtual fence tag usage by the software based on the schematic designs
+current hardware uses an ATMEGA 328PB and a nanotron radio for ranging
+Last Edited: 7/22/24
+By: Andrew Carefoot, Jacob Karl
+*/
+//onboard led
+#define LED1 A0
+
+//flash SPI comms
+#define FLASHCS 5
+#define FLASHMISO 12
+#define FLASHMOSI 11
+#define FLASHSCK 13
+
+//nanotron communication pins
+#define NANORST 10
+#define NANOUSARTRX 7  //serial communication from nanotron
+#define NANOUSARTTX 8  //serial communication to nanotron
+#define NANOENABLE A3
+
+//ftdi hardware serial comms
+#define FTDIRX 0  //recieving from ftdi comms
+#define FTDITX 1  //sending to ftdi
+
+//GPS pins
+#define GPSRX 3
+#define GPSTX 4
+
+//shock circuit pins
+#define SHOCK 6
+#define BOOSTEN 2  //shock charge enable
+#define BUZZER A2
+
+//baudrate definitions
+#define NANOBAUD 115200
+#define TAGBAUD 115200
+
+int warnings = 0; //the controling component of tag mechanics. Our behavior changes depending on how manny of these we have.
+bool on_network = false; //control variable that detemines whether or not we perform ranging or searching
+int sleep_time = 490; //how long we sleep for in mS
+int[64] encryption_key; //the 64-bit encryption key we use to decode messages. Empty by default, set when we recieve an encryption message
+int transmission_decay = 0; //counts the number of times we listen for a message but don't recieve one. Once we reach a threshold set on_network to false
+int timeout = 500; //how long we should wait to recieve a message in mS
