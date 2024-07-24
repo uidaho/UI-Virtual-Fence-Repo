6dea191fd9f1b5531a671ba12de491ca3fede158
Jacob-Karl <karljacobammon@gmail.com>
2024-07-24 11:05:18 -0700

    completed stubbing out

--- Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/nanoTron_Beacon_v0.ino
+++ Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/nanoTron_Beacon_v0.ino
@@ -1,9 +1,39 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  //0.1 Initialize hardware
+  //1.0 Initialize Transmission Recieved interupt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //0.2 get [all tags] with cooldown <= present time
+  //0.3 for [tag] in [all cool tags]
+    //0.4 increase [tag.communication attempts] by 1
+    //0.5 if [tag.communication attempts] > [max communication attempts]
+      //0.5.1 remove [tag] from [all tags] and go to 0.8
+    //0.6 if [tag.distance]<=[boundary]&&[tag.distance]>0
+      //0.6.1 {encode and queue} warning message
+      //0.6.2 set [tag.warning flag] true and go to 0.8
+    //0.7 if [tag.warning flag] == true
+      //0.7.1 {encode and queue} reset message and go to 0.8
+    //0.8 calculate [tag.cooldown timestamp] based on [tag.distance]
+    //0.9 {encode and queue} sleep message based on [tag.cooldown timestamp]
+  //0.10 broadcast [Message]
+  //0.11 clear [Message]
+  //0.12 Sleep until atleast one [tag] will have [tag.cooldown timestamp] <=0
+
+}
+
+//This is the interupt function that we call when we recieve a transmission
+void txRecieve(){
+  //1.1 read transmission
+  //1.2 if transmission is a ranging result
+    //1.2.1 update [tag.distance] for relavant [tag] and go to 1.4
+  //1.3 if transmission is a new tag message
+    //1.3.1 create [tag] object with transmitted ID and add [tag] to [all tags]
+    //1.3.2 {encode and queue} encryption key message
+  //1.4 set [tag.communication attempts] to 0
+}
+
+//This is the function that CDMA encodes messages and adds them to the global [message].
+void encodeAndQueue(){
 
 }
--- Tag/nanoTron_Tag/nanoTron_Tag/nanoTron_Tag.ino
+++ Tag/nanoTron_Tag/nanoTron_Tag/nanoTron_Tag.ino
@@ -1,9 +1,36 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  //2.1 Initialize hardware
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //2.2 if [On Network] == true
+    //2.2.1 Listen to radio
+    //2.2.2 if recieved message within [Timeout]
+      //2.2.2.1 Decrypt message & set [Transmission Decay] to 0
+      //2.2.2.2 if message for us (Because CDMA broadcasts messages to all tags we can recieve a message with no content it it for us. Decoding a message like this should result in an empty message.)
+        //2.2.2.2.1 set [Sleep Time] based on message
+        //2.2.2.2.2 if message is warning
+          //2.2.2.2.2.1 Increment [warnings] by 1
+          //2.2.2.2.2.2 if [warnings] >= 1 && [warnings <= 10]
+            //2.2.2.2.2.2.1 Beep
+            //2.2.2.2.2.2.2 if [warnings] > 5
+              //2.2.2.2.2.2.2.1 Shock
+            //2.2.2.2.2.2.3 go to 2.5
+          //2.2.2.2.2.3 go to 2.5
+        //2.2.2.2.3 if message is reset
+          //2.2.2.3.1 set [warnings] to 0
+        //2.2.2.2.4 go to 2.5
+      //2.2.2.3 go to 2.5
+    //2.2.3 Increment [Transmission Decay] by 1
+    //2.2.4 if [Transmission Decay] > 10
+      //2.2.4.1 set [On Network] false
+    //2.2.5 go to 2.5
+  //2.3 Listen to Radio
+  //2.4 if any network traffic is detected
+    //2.4.1 Transmit addition message
+    //2.4.2 Listen to Radio
+    //2.4.3 if recieved encryption message within [timeout]
+      //2.4.3.1 set [Encryption Key] based on message
+      //2.4.3.2 set [On Network] true
+  //2.5 Sleep for [sleep time]
 }
