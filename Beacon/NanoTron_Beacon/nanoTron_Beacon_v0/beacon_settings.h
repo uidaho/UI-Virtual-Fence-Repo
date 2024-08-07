d5cedc7db043a0cbbca61a0d81502674f357e18b
Jacob-Karl <karljacobammon@gmail.com>
2024-08-07 11:19:19 -0700

    interupt and supporting funcs

--- Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/beacon_settings.h
+++ Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/beacon_settings.h
@@ -41,20 +41,30 @@ By: Matthew Shryock, Jacob Karl
 #define ANALOGAUX4 A11
 #define ANALOGAUX5 A12
 
+//Structures to allow int arrays to be passed and returned from functions easier. Mostly used in the CDMA functions.
+struct array8 {
+  int array[8];
+};
+struct array32{
+  int array[32];
+};
+struct array256{
+  int array[256];
+};
 
 struct Tag {
   String ID; //the unique ID of this tag
   float distance = -1; //the distance of the tag from the boundary when we ranged it last. Initialized to -1 to prevent us from warning a tag that just entered the network.
   int cooldown_timestamp = 0; //the next time we know the tag will be awake. In mS ahead of current time (also in mS). i.e. if current time is 5000mS and we know that the tag will be awake in 2000mS this variable will be 7000mS.
-  int[64] encryption_key; //this tags unique encryption_key. One of the 64 encryption keys from the encryption_key_table.
+  int encryption_key[32]; //this tags unique encryption_key. One of the 32 encryption keys from the encryption_key_table.
   bool warning_flag = false; //a switch that tells us whether or not ro sent the reset signal rather than do nothing when a tag is outside the boundary.
   int communication_attempts = 0; //the number of times we have tried to communicate with this tag without getting a response.
-}
+};
 
 float boundary = 100.0; //the distance from the beacon that tags will start to recieve warnings.
-Tag[] all_tags; //a list of tag structures that constitutes all of the tags on the network.
-int[256] message; //the CDMA encoded message that we broadcast at the end of the main loop.
-int[64][64] encryption_key_table; //a set of 64 orthoganal binary encryption codes (Walsh matricies, maybe, I need to look into this).
+Tag all_tags[32]; //a list of tag structures that constitutes all of the tags on the network.
+Tag all_cool_tags[32]; //a subset of all_tags that contains tags with cooldown_timestamp < current time.
+array256 message; //the CDMA encoded message that we broadcast at the end of the main loop.
+int encryption_key_table[32][32]; //a set of 32 orthoganal binary encryption codes (Walsh matricies, maybe, I need to look into this).
 int max_communication_attempts = 10; //the number of times we will try and communicate with a tag before we remove it from the network.
-
-
+int number_of_tags = 0; //How many tags are on the network. Also serves as an index for the end of all_tags[].
--- Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/nanoTron_Beacon_v0.ino
+++ Beacon/NanoTron_Beacon/nanoTron_Beacon_v0/nanoTron_Beacon_v0.ino
@@ -1,6 +1,15 @@
+#include "beacon_settings.h"
+
 void setup() {
   //0.1 Initialize hardware
+  Serial.begin(115200);
+  Serial2.begin(115200);
+  Serial2.println("SNID 000000000001");
+  Serial2.println("EDAN 1");
+  delay(1);
+  Serial2.println("EDNI 1");
   //1.0 Initialize Transmission Recieved interupt
+  attachInterrupt(digitalPinToInterrupt(NANORX), txRecieve, RISING);
 }
 
 void loop() {
@@ -25,15 +34,50 @@ void loop() {
 //This is the interupt function that we call when we recieve a transmission
 void txRecieve(){
   //1.1 read transmission
+  char c;
+  String reading = "";
+  while(Serial2.available()){
+    c = Serial2.read();
+    reading += c;
+  }
   //1.2 if transmission is a ranging result
+  if(reading.substring(0,3) == "*RRN"){
     //1.2.1 update [tag.distance] for relavant [tag] and go to 1.4
+    all_tags[getTag(reading.substring(5,16))].distance = reading.substring(33,38).toInt();  
+  }
   //1.3 if transmission is a new tag message
+  else if(reading.substring(0,3) == "*DNO"){
     //1.3.1 create [tag] object with transmitted ID and add [tag] to [all tags]
-    //1.3.2 {encode and queue} encryption key message
+    String newID = reading.substring(4);
+    Tag newTag;
+    newTag.ID = newID;
+    number_of_tags++;
+    all_tags[number_of_tags] = newTag;
+    
+    //1.3.2 direct transmit encryption key message
+    Serial2.print("SDAT 0 ");
+    Serial2.print(newID);
+    Serial2.print(" 01 ");
+    if(number_of_tags<10){
+      Serial.println("0"+number_of_tags);
+    } else {
+      Serial.println(number_of_tags);
+    }
+  }
   //1.4 set [tag.communication attempts] to 0
 }
 
 //This is the function that CDMA encodes messages and adds them to the global [message].
-void encodeAndQueue(){
+void encodeAndQueue(int key[32], int new_message[8]){
 
+}
+
+//Returns the index in all_tags of tag with specified ID
+//This function currently has no error handling, so for now don't call this unless you are certain that the tag will exist.
+int getTag(String getID){
+  for(int i=0; i<32; i++){
+    if(all_tags[i].ID == getID){
+      return i;
+    }
+  }
 }
