7d971fbee4e837aaf5ced8b03f5218e6e862764c
shry9735 <triguyprogramming@gmail.com>
2024-08-12 08:55:54 -0700

    Added adc turn off, readability changes

--- lib/liblowpower/liblowpower.cpp
+++ lib/liblowpower/liblowpower.cpp
@@ -3,8 +3,9 @@
 int init_low_power_328p(){
     cli();//disable interrupts during set-up
     MCUSR = 0; //reset status register
-    WDTCSR |= 0b00011000; // using an OR leaves most bits unchanged while setting WDCE and WDE for config mode. See datasheet for details on those.
-    WDTCSR =  0b01000000 | 0b100001; //WDIE - enable interrupt, WDE - disable reset on interrupt. the right half sets the delay to 8 seconds.
+    //turn on WDT and set to 8 seconds
+    wdt_init();
+    disable_adc();
     sei();//enable interrupts
 }
 
@@ -18,4 +19,23 @@ void deep_sleep_328p(int minutes_to_sleep){
         sleep_mode();//This should enable sleep, invoke sleep, then disable sleep
     }
     cli();//disable interrupts
+}
+
+void disable_adc(){
+    // Disable the ADC by setting the ADEN bit (bit 7)  of the
+    // ADCSRA register to zero.
+    ADCSRA = ADCSRA & B01111111;
+
+    // Disable the analog comparator by setting the ACD bit
+    // (bit 7) of the ACSR register to one.
+    ACSR = B10000000;
+
+    // Disable digital input buffers on all analog input pins
+    // by setting bits 0-5 of the DIDR0 register to one.
+    DIDR0 = DIDR0 | B00111111;
+}
+
+void wdt_init(){
+    WDTCSR |= 0b00011000; // using an OR leaves most bits unchanged while setting WDCE and WDE for config mode. See datasheet for details on those.
+    WDTCSR =  0b01000000 | 0b100001; //WDIE - enable interrupt, WDE - disable reset on interrupt. the right half sets the delay to 8 seconds.
 }
--- lib/liblowpower/liblowpower.hpp
+++ lib/liblowpower/liblowpower.hpp
@@ -2,3 +2,5 @@
 
 void init_low_power_328p();
 void deep_sleep_328p();
+void disable_adc();
+void wdt_init();
