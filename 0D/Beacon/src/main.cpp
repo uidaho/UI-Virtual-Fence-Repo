0c44ebd343c25af762eab2b70d27e432d1f0d472
shry9735 <triguyprogramming@gmail.com>
2024-09-04 06:45:54 -0700

    0D being used for test

--- 0D/Beacon/src/main.cpp
+++ 0D/Beacon/src/main.cpp
@@ -3,14 +3,20 @@
 
 // put function declarations here:
 
-
+nanotron my_nanotron;
+String OtherID="";
 void setup() {
   // put your setup code here, to run once:
   pinMode(30,OUTPUT);
+  pinMode(31,OUTPUT);
+  pinMode(32,OUTPUT);
+  pinMode(33,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(30,!digitalRead(30));
+  my_nanotron.read_other_input_voltage();
   delay(1000);
 }
