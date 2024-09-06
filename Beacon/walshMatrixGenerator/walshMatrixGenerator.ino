f0ba344c6400fa36f8ade2a88fa10e353f7cba17
Jacob-Karl <karljacobammon@gmail.com>
2024-09-06 10:45:22 -0700

    walsh compile untested

--- Beacon/walshMatrixGenerator/walshMatrixGenerator.ino
+++ Beacon/walshMatrixGenerator/walshMatrixGenerator.ino
@@ -1,5 +1,14 @@
+int walsh_key[256];
+
 void setup() {
   // put your setup code here, to run once:
+  for(int i=0; i<16; i++){
+    Walsh_row(i,16);
+    for(int j=0; j<16; j++){
+      Serial.print(walsh_key[j]+", ");
+    }
+    Serial.println();
+  }
 
 }
 
@@ -11,24 +20,25 @@ void loop() {
 //for walsh matrix of height 16, we have 4 elements (2^4=16) but the first is always true
 // |4  3  2  1
 // |----------
-//1|T  T  T
-//2|T  T  F
-//3|T  F  T
-//4|T  F  F
-//5|F  T  T
-//6|F  T  F
-//7|F  F  T
-//8|F  F  F
+//1|T  T  T  T
+//2|T  T  F  T
+//3|T  F  T  T
+//4|T  F  F  T
+//5|F  T  T  T
+//6|F  T  F  T
+//7|F  F  T  T
+//8|F  F  F  T
 
-int[] Walsh_row(int row, int height){
+void Walsh_row(int row, int height){
   int wRow[height];
   //mod the row number to determ  ine whether we're reflecting the first or second row of the original
+  //This is the first element that is always true. Because the fractal is square rows are reflections of either the first or second row of the seed.
   if(row%2 == 1){
-    wRow[0] = 1;
-    wRow[1] = 1;
+    walsh_key[0] = 1;
+    walsh_key[1] = 1;
   } else {
-    wRow[0] = 1;
-    wRow[1] = 0;
+    walsh_key[0] = 1;
+    walsh_key[1] = 0;
   }  
   //Use logical operators to determine the truth value of each bit in sequence and reflect accordingly
   int bitGet = 1;
@@ -39,17 +49,17 @@ int[] Walsh_row(int row, int height){
     int rowBit = rowHalf & bitGet;
     if(rowBit == 0){//True reflection
       for(int j=0; i<wRowPos; j++){
-        wRow[wRowPos+j] = wRow[j];
-        wRow = wRow*2;
+        walsh_key[wRowPos+j] = walsh_key[j];
+        wRowPos = wRowPos*2;
       }
     }
     else{//False reflection
       for(int j=0; j<wRowPos; j++){
-        wRow[wRowPos+j] = abs(wRow[j]-1);
-        wRow = wRow*2;
+        walsh_key[wRowPos+j] = abs(walsh_key[j]-1);
+        wRowPos = wRowPos*2;
       }
     }
     bitGet << 1;
   }
-  return wRow[];
+  return &wRow;
 }
